281eafabca2e7ac8721cb38118c117179c2fc7fa
tsalim.13 <gotlm13@gmail.com>
2018-03-19 00:39:07 +0100

    list sujet jsp et controller

---
+++ planningMaker/src/main/java/Controller/ListeSujet.java
@@ -0,0 +1,44 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.EtudiantDaoImpl;
+import dao.SujetDaoImpl;
+import model.Sujet;
+
+public class ListeSujet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ListeSujet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		EtudiantDaoImpl etudiant = new EtudiantDaoImpl();
+		List<String> Specilaite = etudiant.findSpecialite();
+		request.setAttribute("specialite", Specilaite);
+		
+		this.getServletContext().getRequestDispatcher("/liste_sujet.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String specialite = request.getParameter("spec");
+		Sujet sujet = new Sujet() ;
+		sujet.setSpecialite(specialite);
+
+		SujetDaoImpl sujetdao = new SujetDaoImpl();
+		List<Sujet> sujets = sujetdao.findBySpecialite(sujet);
+		request.setAttribute("sujets", sujets);
+
+		this.getServletContext().getRequestDispatcher("/listeSujet.jsp").forward(request, response);
+	}
+
+}
