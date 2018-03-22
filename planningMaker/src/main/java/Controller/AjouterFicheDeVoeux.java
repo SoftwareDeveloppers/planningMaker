bb4d0ab34b55def80504631b04c1ca1bfd75d917
inteex <inteex13@gmail.com>
2018-03-22 21:17:03 +0100

    ficheDeVoeux/notif added,redirection vers Login added

--- planningMaker/src/main/java/Controller/AjouterFicheDeVoeux.java
+++ planningMaker/src/main/java/Controller/AjouterFicheDeVoeux.java
@@ -24,28 +24,34 @@ public class AjouterFicheDeVoeux extends HttpServlet {
 			throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
-		int idEtudiant = (Integer) session.getAttribute("idEtudiant");
+		if (session.getAttribute("idEtudiant") == null) {
 
-		FicheDeVoeuxDaoImpl ficheDao = new FicheDeVoeuxDaoImpl();
-
-		if (ficheDao.nbrSujeuts(idEtudiant) != 5) {
-
-			EtudiantDaoImpl etudiant = new EtudiantDaoImpl();
-			List<String> Specilaite = etudiant.findSpecialite();
-			request.setAttribute("specialite", Specilaite);
-
-			remplie = false;
-			request.setAttribute("remplie", remplie);
-			this.getServletContext().getRequestDispatcher("/fiche-de-voeux.jsp").forward(request, response);
-
-		} else {
-			FicheDeVoeuxDaoImpl fichedao= new FicheDeVoeuxDaoImpl();
+			response.sendRedirect("Login");
 			
-			remplie = true;
-			request.setAttribute("remplie", remplie);
-			request.setAttribute("sujetsChoisi", fichedao.listSujets(idEtudiant));
-			this.getServletContext().getRequestDispatcher("/fiche-de-voeux.jsp").forward(request, response);
+		} else {
+			int idEtudiant = (Integer) session.getAttribute("idEtudiant");
 
+			FicheDeVoeuxDaoImpl ficheDao = new FicheDeVoeuxDaoImpl();
+
+			if (ficheDao.nbrSujeuts(idEtudiant) != 5) {
+
+				EtudiantDaoImpl etudiant = new EtudiantDaoImpl();
+				List<String> Specilaite = etudiant.findSpecialite();
+				request.setAttribute("specialite", Specilaite);
+
+				remplie = false;
+				request.setAttribute("remplie", remplie);
+				this.getServletContext().getRequestDispatcher("/fiche-de-voeux.jsp").forward(request, response);
+
+			} else {
+				FicheDeVoeuxDaoImpl fichedao = new FicheDeVoeuxDaoImpl();
+
+				remplie = true;
+				request.setAttribute("remplie", remplie);
+				request.setAttribute("sujetsChoisi", fichedao.listSujets(idEtudiant));
+				this.getServletContext().getRequestDispatcher("/fiche-de-voeux.jsp").forward(request, response);
+
+			}
 		}
 
 	}
@@ -61,30 +67,25 @@ public class AjouterFicheDeVoeux extends HttpServlet {
 		int idSujet4 = Integer.parseInt(request.getParameter("sujet4"));
 		int idSujet5 = Integer.parseInt(request.getParameter("sujet5"));
 
+		FicheDeVoeux fiche1 = new FicheDeVoeux(idEtudiant, idSujet1);
+		FicheDeVoeux fiche2 = new FicheDeVoeux(idEtudiant, idSujet2);
+		FicheDeVoeux fiche3 = new FicheDeVoeux(idEtudiant, idSujet3);
+		FicheDeVoeux fiche4 = new FicheDeVoeux(idEtudiant, idSujet4);
+		FicheDeVoeux fiche5 = new FicheDeVoeux(idEtudiant, idSujet5);
 
-			FicheDeVoeux fiche1 = new FicheDeVoeux(idEtudiant, idSujet1);
-			FicheDeVoeux fiche2 = new FicheDeVoeux(idEtudiant, idSujet2);
-			FicheDeVoeux fiche3 = new FicheDeVoeux(idEtudiant, idSujet3);
-			FicheDeVoeux fiche4 = new FicheDeVoeux(idEtudiant, idSujet4);
-			FicheDeVoeux fiche5 = new FicheDeVoeux(idEtudiant, idSujet5);
+		FicheDeVoeuxDaoImpl ficheDao1 = new FicheDeVoeuxDaoImpl();
+		FicheDeVoeuxDaoImpl ficheDao2 = new FicheDeVoeuxDaoImpl();
+		FicheDeVoeuxDaoImpl ficheDao3 = new FicheDeVoeuxDaoImpl();
+		FicheDeVoeuxDaoImpl ficheDao4 = new FicheDeVoeuxDaoImpl();
+		FicheDeVoeuxDaoImpl ficheDao5 = new FicheDeVoeuxDaoImpl();
 
-			FicheDeVoeuxDaoImpl ficheDao1 = new FicheDeVoeuxDaoImpl();
-			FicheDeVoeuxDaoImpl ficheDao2 = new FicheDeVoeuxDaoImpl();
-			FicheDeVoeuxDaoImpl ficheDao3 = new FicheDeVoeuxDaoImpl();
-			FicheDeVoeuxDaoImpl ficheDao4 = new FicheDeVoeuxDaoImpl();
-			FicheDeVoeuxDaoImpl ficheDao5 = new FicheDeVoeuxDaoImpl();
+		ficheDao1.create(fiche1);
+		ficheDao2.create(fiche2);
+		ficheDao3.create(fiche3);
+		ficheDao4.create(fiche4);
+		ficheDao5.create(fiche5);
 
-			ficheDao1.create(fiche1);
-			ficheDao2.create(fiche2);
-			ficheDao3.create(fiche3);
-			ficheDao4.create(fiche4);
-			ficheDao5.create(fiche5);
-			
-			this.doGet(request, response);
-
-		
-		
-		
+		this.doGet(request, response);
 
 	}
 
--- planningMaker/src/main/webapp/assets/form.js
+++ planningMaker/src/main/webapp/assets/form.js
@@ -15,7 +15,45 @@ $(function() {
 			return true ; 
 		}else
 		{
-			alert ("Vous devez choisir des sujets different!!");
+			$.growl({
+	            icon: '',
+	            title: 'Attention ',
+	            message: 'vous devez choisir des sujets different . ',
+	            url: ''
+	        },{
+	            element: 'body',
+	            type: 'danger',
+	            allow_dismiss: true,
+	            placement: {
+	                from: 'top',
+	                align: 'right'
+	            },
+	            offset: {
+	                x: 30,
+	                y: 30
+	            },
+	            spacing: 10,
+	            z_index: 999999,
+	            delay: 4500,
+	            timer: 1000,
+	            url_target: '_blank',
+	            mouse_over: false,
+	            animate: {
+	                enter: 'animated fadeInRight',
+	                exit: 'animated fadeOutRight'
+	            },
+	            icon_type: 'class',
+	            template: '<div data-growl="container" class="alert" role="alert">' +
+	            '<button type="button" class="close" data-growl="dismiss">' +
+	            '<span aria-hidden="true">&times;</span>' +
+	            '<span class="sr-only">Close</span>' +
+	            '</button>' +
+	            '<span data-growl="icon"></span>' +
+	            '<span data-growl="title"></span>' +
+	            '<span data-growl="message"></span>' +
+	            '<a href="#" data-growl="url"></a>' +
+	            '</div>'
+	        });
 		return false;
 		
 		}
