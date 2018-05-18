83e51c529c3586f385767ccc866a8e05355f9f48
tsalim.13 <gotlm13@gmail.com>
2018-05-18 03:11:19 +0100

    affectation dao et controller

--- planningMaker/src/main/java/dao/AffectationDaoImpl.java
+++ planningMaker/src/main/java/dao/AffectationDaoImpl.java
@@ -1,12 +1,15 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mysql.jdbc.PreparedStatement;
 
 import model.Affectation;
+import model.Sujet;
 
 public class AffectationDaoImpl implements AffectationDao{
 	
@@ -31,6 +34,36 @@ public class AffectationDaoImpl implements AffectationDao{
 
 			return false;
 		}
+	}
+	
+	public Affectation findBySujet(int id) {
+		Connection conn=DbConnect.connect();
+
+		String sql = "Select * FROM affectation WHERE id_Sujet=?";
+		PreparedStatement ps;
+		ResultSet rs = null ;
+		Affectation affectation = null ;
+		
+		try {
+			ps = (PreparedStatement) conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			rs=ps.executeQuery();
+			while (rs.next()){
+				affectation = new Affectation(
+						rs.getInt("id"),
+						rs.getInt("id_Etudiant"), 
+						rs.getInt("id_Sujet")
+						);
+			}	
+			conn.close();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		
+		return affectation;
 	}
 
 }
